89a858a8f4daa7dbcad65c0de9bf63ab59d37ebe
Lourine <lourine.millicent@gmail.com>
2021-04-16 00:20:05 +0300

    create polls app with relevant files

---
+++ polls/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from .apiviews import PollList, PollDetail,ChoiceList, CreateVote, UserCreate, LoginView
+from rest_framework.routers import DefaultRouter
+from .apiviews import PollViewSet
+router = DefaultRouter()
+router.register('polls', PollViewSet, basename='polls')
+
+
+urlpatterns = [
+    path("login/", LoginView.as_view(), name="login"),
+    path("users/", UserCreate.as_view(), name="user_create"),
+    path("polls/", PollList.as_view(), name="polls_list"),
+    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),
+    path("polls/<int:pk>/choices/", ChoiceList.as_view(), name="choice_list"),
+    path("polls/<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(), name="create_vote"),
+]
+urlpatterns += router.urls
